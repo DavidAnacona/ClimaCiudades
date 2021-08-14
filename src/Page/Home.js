fb0e1f300f8b7e815366620354e413119a755ccb
bue221 <camiloplaza3@gmail.com>
2021-08-13 22:05:23 -0500

    add 3 days fetch and loader

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,63 +1,75 @@
-import { useState, useEffect } from 'react';
-import { API_KEY, API_URL } from 'Components/settings';
-import Card from 'Components/Card/index'
-import CardCities from 'Components/CardCities/index';
+import { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "Components/settings";
+import Card from "Components/Card/index";
+import CardCities from "Components/CardCities/index";
+import "./style.css";
 
 const Home = () => {
+  const [Bogota, setBogota] = useState(null);
+  const [Paris, setParis] = useState(null);
+  const [Barcelona, setBarcelona] = useState(null);
+  const [days, setDays] = useState(null);
 
-    const [Bogota, setBogota] = useState([])
-    const [Paris, setParis] = useState([])
-    const [Barcelona, setBarcelona] = useState([])
+  const fetchOperation = async (url, set) => {
+    try {
+      const responsive = await fetch(url);
+      const data = await responsive.json();
+      set(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    useEffect(()=>{
+  useEffect(() => {
+    if (days?.list) setDia(new Date(days.list[0].dt_txt).getDate());
+  }, [days]);
+  ///13
+  const [dia, setDia] = useState(new Date().getDate());
 
-        const fromApiResponseBogota = async () =>{
-            try {
-                const responsive =  await fetch(`${API_URL}Bogota&appid=${API_KEY}&units=metric`)
-                const data = await responsive.json();
-                console.log(data)
-                setBogota(data)
-            } catch (e) {
-                console.log(e)
-            }
-        };
+  const dia1 = days?.list?.filter(
+    (i) => new Date(i.dt_txt).getDate() === dia
+  )[0];
+  const dia2 = days?.list?.filter(
+    (i) => new Date(i.dt_txt).getDate() === dia + 1
+  )[0];
+  const dia3 = days?.list?.filter(
+    (i) => new Date(i.dt_txt).getDate() === dia + 2
+  )[0];
 
-        const fromApiResponseParis = async () =>{
-            try {
-                const responsive =  await fetch(`${API_URL}Paris,FR&appid=${API_KEY}&units=metric`)
-                const data = await responsive.json();
-                console.log(data)
-                setParis(data)
-            } catch (e) {
-                console.log(e)
-            }
-        };
+  useEffect(() => {
+    fetchOperation(`${API_URL}Bogota&appid=${API_KEY}&units=metric`, setBogota);
+    fetchOperation(
+      `${API_URL}Paris,FR&appid=${API_KEY}&units=metric`,
+      setParis
+    );
+    fetchOperation(
+      `${API_URL}Barcelona&appid=${API_KEY}&units=metric`,
+      setBarcelona
+    );
+    fetchOperation(
+      `https://api.openweathermap.org/data/2.5/forecast?q=bogota&units=metric&appid=${API_KEY}`,
+      setDays
+    );
+  }, []);
 
-        const fromApiResponseBarcelona = async () =>{
-            try {
-                const responsive =  await fetch(`${API_URL}Barcelona&appid=${API_KEY}&units=metric`)
-                const data = await responsive.json();
-                console.log(data)
-                setBarcelona(data)
-            } catch (e) {
-                console.log(e)
-            }
-        };
-
-        fromApiResponseBogota();
-        fromApiResponseParis();
-        fromApiResponseBarcelona();
-        
-    }, [])
-
-    
-    return (
+  return (
+    <>
+      {days && Bogota && Barcelona && Paris ? (
         <div>
-            <Card City={Bogota} />
-            <CardCities City={Paris} />
-            <CardCities City={Barcelona} />
+          {dia1?.weather && <h1>{dia1.weather[0].description}</h1>}
+          <Card City={Bogota} />
+          <CardCities City={Paris} />
+          <CardCities City={Barcelona} />
         </div>
-    )
+      ) : (
+        <div className="papa">
+          <div class="lds-circle">
+            <div></div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
